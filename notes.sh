cd11b4eb14485c7bb2442efdb27f5e136d1aaad1
UntuKemeng <jarbull87@gmail.com>
2025-02-15 18:41:36 +0700

    Fixed filter release

---
+++ notes.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "
+## OrangeFox Recovery Build - Unofficial
+🖥 OrangeFox Branch: ${{ env.FOX_BRANCH }}
+📱 Device: ${{ env.DEVICE }}
+📝 CodeName: ${{ env.CODENAME }}
+📂 Size: ${{ env.ORF_SIZE }}
+👩‍💻 Top Commit: ${{ env.DT_COMMIT }}
+📕 MD5: ${{ env.ORF_MD5 }}
+📘 SHA1: ${{ env.ORF_SHA1 }}
+" > ${GITHUB_WORKSPACE}/release-notes.md
